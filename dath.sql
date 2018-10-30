c9b6d148b197acda39b9b87f83807e63c5d43a4f
Eduardo <epontesveiga@hotmail.com>
2018-10-30 11:13:02 -0300

    Agora sim

--- dath.sql
+++ dath.sql
@@ -1,8 +1,8 @@
 create database dath character set = utf8mb4 COLLATE utf8mb4_unicode_ci;
 use dath;
 
-#Tabela com os dados dos usuários
-CREATE TABLE usuario (
+#Usuario/Médico/Hospital
+CREATE TABLE Usuario (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(70) UNIQUE NOT NULL,
@@ -14,19 +14,6 @@ CREATE TABLE usuario (
     sexo ENUM('Masculino', 'Feminino') NOT NULL
 );
 
-#Exacon = Exames e Consultas
-create table Hospital (
-	id int not null auto_increment,
-    nome varchar(50) not null,
-    cep int not null,
-    pais char(2),
-    estado char(2) not null,
-    cidade varchar(60) not null,
-    rua varchar(50) not null,
-    num_predio int not null,
-    primary key(id)
-);
-
 create table Medico(
 	id int not null auto_increment,
     nome varchar(50) not null,
@@ -34,16 +21,66 @@ create table Medico(
     primary key(id)
 );
 
+
+create table Pais(
+	id int not null,
+    id_estado int not null,
+    nome char(2),
+    primary key(id)
+);
+
+create table Estado(
+	id int not null,
+    nome char(2),
+    primary key(id)
+);
+
+create table Cidade(
+	id int not null auto_increment,
+    nome varchar(60),
+    primary key(id)
+);
+
+create table Rua(
+	id int not null auto_increment,
+    nome varchar(60) not null,
+    primary key(id)
+);
+
+create table Endereco(
+	id int not null auto_increment,
+	id_pais int not null,
+    id_Estado int not null,
+    id_cidade int not null,
+    id_rua int not null,
+    num_predio int not null,
+    cep int not null,
+    primary key(id),
+    constraint fk_PaisEndereco foreign key(id_pais) references Pais(id),
+    constraint fk_EstadoEndereco foreign key(id_estado) references Estado(id),
+    constraint fk_CidadeEndereco foreign key(id_cidade) references Cidade(id),
+    constraint fk_RuaEndereco foreign key(id_rua) references Rua(id)
+);
+
+create table Hospital (
+	id int not null auto_increment,
+    nome varchar(50) not null,
+    id_endereco int not null,
+    primary key(id),
+    constraint fk_EnderecoHospital foreign key(id_endereco) references Endereco(id)
+);
+
+#Exa/Con
 create table Exacon (
 	id int not null auto_increment,
 	id_hospital int not null,
     id_medico int not null,
-    Horario DATETIME NOT NULL,
-	Id_Usuario int,
+    horario DATETIME NOT NULL,
+	id_usuario int,
     primary key(id),
-    constraint fk_UsuarioExacon foreign key(id_Usuario) references Usuario(id),
-    constraint fk_HospitalExacon foreign key(id_Hospital) references Hospital(id),
-    constraint fk_MedicoExacon foreign key(id_Medico) references Medico(id)
+    constraint fk_UsuarioExacon foreign key(id_usuario) references Usuario(id),
+    constraint fk_HospitalExacon foreign key(id_hospital) references Hospital(id),
+    constraint fk_MedicoExacon foreign key(id_medico) references Medico(id)
 );
 
 create table Exa(
@@ -53,4 +90,3 @@ create table Exa(
     primary key(id),
     constraint fk_ExaCon foreign key(id_exacon) references Exacon(id)
 );
-#ALTER TABLE exacon AUTO_INCREMENT = 1;
